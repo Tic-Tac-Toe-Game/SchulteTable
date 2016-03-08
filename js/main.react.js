362364b0fe8e155cafb1c7d12b70cdc64c8dfd81
ReenExe <ipourri@gmail.com>
2016-03-09 00:21:44 +0200

    progress

--- js/main.react.js
+++ js/main.react.js
@@ -4,18 +4,28 @@ define([
     'generator',
     'app.config'
 ], function (React, ReactDOM, Generator, AppConfig) {
+    var CellComponentConfig = {
+        className: AppConfig.selector.cellDefaultClassName
+    };
+    var CellClass = React.createClass({
+        render: function () {
+            return React.createElement(
+                'td',
+                CellComponentConfig,
+                this.props.children
+            );
+        }
+    });
+
     var TableClass = React.createClass({
         render: function () {
             var generator = Generator(AppConfig.size);
             var rows = [];
-            var columnConfig = {
-                className: AppConfig.selector.cellDefaultClassName
-            };
             for (var y = 1; y <= AppConfig.size; y++) {
                 var columns = [];
                 for (var x = 1; x <= AppConfig.size; x++) {
                     columns.push(
-                        React.createElement('td', columnConfig, generator())
+                        React.createElement(CellClass, null, generator())
                     );
                 }
                 rows.push(
