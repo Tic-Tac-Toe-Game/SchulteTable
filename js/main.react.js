e8e049a0ce8b718f03ee9e03cd12a75690f45327
ReenExe <ipourri@gmail.com>
2016-03-08 23:32:51 +0200

    render `Table`

--- js/main.react.js
+++ js/main.react.js
@@ -2,8 +2,29 @@ define([
     'react',
     'react-dom'
 ], function (React, ReactDOM) {
+    var  size = 5;
+
+    var TableClass = React.createClass({
+        render: function () {
+            var rows = [];
+            for (var y = 1; y <= size; y++) {
+                var columns = [];
+                for (var x = 1; x <= size; x++) {
+                    columns.push(
+                        React.createElement('td', null, 'A')
+                    );
+                }
+                rows.push(
+                    React.createElement('tr', null, columns)
+                );
+            }
+
+            return React.createElement('table', null, rows);
+        }
+    });
+
     ReactDOM.render(
-        React.createElement('div'),
+        React.createElement(TableClass),
         document.getElementById('js-schulte')
     );
 });
