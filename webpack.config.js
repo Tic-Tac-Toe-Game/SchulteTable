16356fa2325108c894e3bfedc6b4b808edfe2f97
ReenExe <ipourri@gmail.com>
2016-06-08 20:09:02 +0300

    default with `Webpack`

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+var webpack = require('webpack');
+
+var environment = process.env.NODE_ENV || 'development';
+
+var plugins = environment === 'production' ? [
+    new webpack.DefinePlugin({
+        'process.env':{
+            'NODE_ENV': JSON.stringify(environment)
+        }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false
+        }
+    })
+] : [];
+
+module.exports = {
+    context: __dirname + '/js/react',
+    entry: {
+        'app': './components/app'
+    },
+
+    output: {
+        path: __dirname + '/js/public',
+        filename: '[name].js'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                include: __dirname + '/js',
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+
+    plugins: plugins,
+    devtool: 'source-map',
+
+    resolve: {
+        alias: {
+            'st' : __dirname + '/js/react'
+        }
+    }
+};
