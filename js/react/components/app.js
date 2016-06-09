525c2802242e477a044b7b9d7bfbc03a687db9b3
ReenExe <ipourri@gmail.com>
2016-06-09 19:13:53 +0300

    progress

--- js/react/components/app.js
+++ js/react/components/app.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Table from 'st/components/table';
 
 render(
-    <div />,
+    <Table />,
     document.getElementById('js-schulte')
 );
